0a3df98860fae23e376317046129668197e9a55a
Vanessa Sergeon <vanessa@openfin.co>
2017-08-09 16:05:50 -0400

    Remove broken tests and test setup.

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'], exclude: /node_modules/ },
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'], exclude: /node_modules/ },
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
