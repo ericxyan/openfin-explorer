f9fd894bc8a396b5f7872c5ae2f088954e8974c0
dcchuck <chuck.danielsson@gmail.com>
2017-08-23 16:50:50 -0400

    Frameless window

--- public/react/components/Page-Header.tsx
+++ public/react/components/Page-Header.tsx
@@ -3,6 +3,11 @@ import * as React from 'react';
 export default class PageHeader extends React.Component<PageHeaderProps> {
     constructor(props: PageHeaderProps) {
         super(props);
+        this.closeApp = this.closeApp.bind(this);
+    }
+
+    private closeApp() {
+        fin.desktop.Application.getCurrent().close();
     }
 
     public render() {
@@ -11,6 +16,7 @@ export default class PageHeader extends React.Component<PageHeaderProps> {
                 <h2>
                     <i className='material-icons'>{this.props.sectionIcon} </i>
                     {this.props.header}
+                    <span className='closebutton' onClick={this.closeApp}>X</span>
                 </h2>
                 <h5>{this.props.subHeader}</h5>
                 <p>{this.props.description}</p>
