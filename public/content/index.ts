502f8cbc93614987089d9485580a95b96aae8d3b
dcchuck <chuck.danielsson@gmail.com>
2017-09-21 15:35:13 -0400

    Put the communication demo back, reorder sidenav

--- public/content/index.ts
+++ public/content/index.ts
@@ -3,7 +3,8 @@ import windows from './windows';
 import notifications from './notifications';
 import system from './system';
 import applications from './applications';
+import communication from './communication';
 
-const content = Array.prototype.concat(home, windows, notifications, system, applications);
+const content = Array.prototype.concat(home, applications, windows, communication, notifications, system);
 
 export default content;
